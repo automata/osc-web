585a2854be894fa1ad78c9f9ca4ddded3ee7ca0f
Vilson Vieira <vilson@void.cc>
2011-02-21 03:06:10 -0300

    now we can receive OSC messages on the browser too

--- osc-web-server.js
+++ osc-web-server.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
     io = require('socket.io');
 
 require.paths.unshift(__dirname + '/node-osc/lib');
-// FIXME: commit to node-osc and try to eliminate jspack dependency
+
 var osc = require('osc');
 // FIXME: implement the OSCServer on node-osc, so we will not need dgram here
 var dgram = require('dgram');
@@ -22,7 +22,7 @@ server.listen(4040);
 var io = io.listen(server);
 
 io.on('connection', function(client){
-    var OSCclient = new osc.Client(17144, '127.0.0.1');
+    var OSCclient = new osc.Client(11720, '127.0.0.1');
 
     // tell to lp that it should send messages to us at localhost:4343
     OSCmsg = new osc.Message('/lp/dest');
@@ -30,21 +30,18 @@ io.on('connection', function(client){
     OSCclient.send(OSCmsg);
 
     // so let's start to listen on 4343
-    var OSCserver = dgram.createSocket("udp4");
-        
-    OSCserver.bind(4343);
+    var OSCserver = new osc.Server(4343, '127.0.0.1');
 
-    // when we receive messages on 4343, have to pass to the browser
-    OSCserver.on("message", function (msg, rinfo) {
-        console.log("server got: " + msg + " from " +
-                    rinfo.address + ":" + rinfo.port);
-        client.send({message: 'got message from OSC'});
+    OSCserver.addMsgHandler('/lp/matrix', function (args) {
+        client.send({message: args});
     });
 
-    OSCserver.on("listening", function () {
-        var address = OSCserver.address();
-        console.log("server listening " +
-                    address.address + ":" + address.port);
+    OSCserver.addMsgHandler('/lp/ctrl', function (args) {
+        client.send({message: '/lp/ctrl ' + args});
+    });
+
+    OSCserver.addMsgHandler('/lp/scene', function (args) {
+        client.send({message: '/lp/scene ' + args});
     });
 
     client.broadcast({ connection: client.sessionId});
@@ -65,4 +62,3 @@ io.on('connection', function(client){
         client.broadcast({ disconnection: client.sessionId});
     });
 });
-
--- test-app/test-app.js
+++ test-app/test-app.js
@@ -1,44 +1,56 @@
-//
-// socket.io
-//
-
-// create the socket to the local OSC server
-var socket = new io.Socket("localhost", {port: 4040, rememberTransport: false});
-
-// NOTE: we can create sockets to remote hosts too!!!
-
-// connect to the socket
-socket.connect();
-
-// when a message from server was received...
-socket.on('message', function(obj) { 
-    var status = document.getElementById('status');
-    var el = document.createElement('p');
-    if ('connection' in obj) {
-        el.innerHTML = 'user ' + obj.connection + ' connected';
-    } else if ('disconnection' in obj) {
-        el.innerHTML = 'user ' + obj.disconnection + ' disconnected';
-    } else if ('message' in obj) {
-        el.innerHTML = 'user sends: ' + obj.message;
-    }
-    // DEBUG: console.log(obj);
-    status.appendChild(el);
-});      
-
-//
-// UI
-//
-
 $(function() {
+
+    //
+    // UI
+    //
+
     $( ".tonematrix" ).draggable();
     $( "input:checkbox", ".tonematrix").button().click(function () {
         var id = $(this).attr('id');
-        console.log(id);
         if ($(this).attr('checked')) {
             socket.send(id[1] + ' ' + id[2] + ' 127');
         } else {
             socket.send(id[1] + ' ' + id[2] + ' 0');
         }
     });
+
+    //
+    // socket.io
+    //
+    
+    // create the socket to the local OSC server
+    var socket = new io.Socket("localhost", {port: 4040, rememberTransport: false});
+
+    // NOTE: we can create sockets to remote hosts too!!!
+
+    // connect to the socket
+    socket.connect();
+
+    // when a message from server was received...
+    socket.on('message', function(obj) { 
+        // var status = document.getElementById('status');
+        // var el = document.createElement('p');
+        if ('connection' in obj) {
+            // el.innerHTML = 'user ' + obj.connection + ' connected';
+        } else if ('disconnection' in obj) {
+            // el.innerHTML = 'user ' + obj.disconnection + ' disconnected';
+        } else if ('message' in obj) {
+            // el.innerHTML = 'user sends: ' + obj.message;
+
+            var pos = obj.message.toString().split(',');
+            // check the checkbox...
+            if (pos[2] == '127') {
+                $('#c' + pos[0] + pos[1]).attr('checked', true);
+            } else {
+                $('#c' + pos[0] + pos[1]).attr('checked', false);
+            }
+            $('#c' + pos[0] + pos[1]).button('refresh');
+
+            // ... and check the lp
+            socket.send(pos[0] + ' ' + pos[1] + ' ' + pos[2]);
+        }
+        // status.appendChild(el);
+    });      
+
 });
 
