99f74cda50330c8f0d6d543f19a2a40b778c5873
Lukas Lange <lu.lange@ndr.de>
2020-03-11 14:05:27 +0100

    V0.1

--- bridge.js
+++ bridge.js
@@ -7,21 +7,15 @@ io.on('connection', function (socket) {
   socket.on('config', function (obj) {
     console.log('config', obj);
     oscServer = new osc.Server(obj.server.port, obj.server.host);
-    oscClient = new osc.Client(obj.client.host, obj.client.port);
-
-    oscClient.send('/status', socket.id + ' connected');
+    console.log(socket.id + " connected");
 
     oscServer.on('message', function(msg, rinfo) {
       socket.emit('message', msg);
-      console.log('sent OSC message to WS', msg, rinfo);
+      console.log(msg);
+      //console.log('sent OSC message to WS', msg, rinfo);
     });
   });
-  socket.on('message', function (obj) {
-    var toSend = obj.split(' ');
-    oscClient.send(...toSend);
-    console.log('sent WS message to OSC', toSend);
-  });
   socket.on("disconnect", function () {
-    oscServer.kill();
+    oscServer.close();
   })
 });
